9f5cf68a65be1b544c517f069cc269e3e73bfb69
mariacbav <maria.c.viana@ufv.br>
2022-10-11 20:33:12 -0300

    Implementação ordem, lógicas de diametro e centralidade comentadas

--- biblioteca/biblioteca_amostra.py
+++ biblioteca/biblioteca_amostra.py
@@ -1,1 +1,23 @@
 #Esse arquivo foi criado só para estruturar a pasta, podem apagar quando criarem outros arquivos na pasta
+
+def ordem(dict_arquivo):
+
+    ordem = dict_arquivo.numVertices
+    return (ordem)
+
+#def diametro():
+    #N = ordem(dict_arquivo)
+    #maior = 0
+    #não tenho que inicializar i = 0?
+    #for i range N
+        #chama a função de excentricidade passando o vertice i
+        #if exentricidade > maior
+            #maior = exentricidade
+    #return (maior)
+
+#def centralidade(vertice x)
+    #N = ordem(dict_arquivo)
+    #ACHO que d(y,x) é a media das distancias (inclue ele?)
+    #se for isso chama distancia dentro de um for percorendo todo vetor vai guardando a soma disso num somatorio
+    #divide o somatorio por N (ou por N-1 se não incluir ele) (para ter a media)
+    #coloca na formula
