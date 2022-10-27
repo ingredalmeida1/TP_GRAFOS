4252ab81862bf12800602f7ccbeb3aeabf3bd88e
ingredalmeida1 <ingred.almeida@ufv.br>
2022-10-27 14:33:36 -0300

    alteracao menu

--- menu/menu.py
+++ menu/menu.py
@@ -1,3 +1,5 @@
+import os
+
 def menu():
     print("="* 60)
     print(3*"\t","MENU")
@@ -8,23 +10,55 @@ def escolhaArq():
     tipo = int(input(">>> "))
     return tipo
 
-def imprimeBiblioteca(vertice, grafo, n):
-    grafo.floydWarshall(vertice)
-    print(f'VÉRTICE ESCOLHIDO: {vertice}')
-    print("\n>>> Ordem do grafo: ", grafo.ordem())
-    print("\n>>> Tamanho do grafo: ", grafo.tamanho())
-    grafo.retornaVizinhos(vertice)
-    grafo.grauVertice(vertice)
-    grafo.sequenciaGraus()
-    print(f'\n>>> Excentricidade do vértice {vertice}: ', grafo.excentricidade(vertice))
-    print("\n>>> Raio do grafo: ", grafo.raio())
-    print("\n>>> Diâmetro do grafo: ", grafo.diametro())
-    print("\n>>> Centro do grafo: ", grafo.centro())
-    print("\n>>> Busca em profundidade: ")
-    print("\n>>> Distância e caminho mínimo\n")
-    for i in range(n):
-        print(f'>>>Caminho minimo entre {vertice} e {i + 1}: {grafo.imprimeCaminho(vertice, i + 1)}')
-    print("\n")
-    grafo.imprimeDistancia(vertice)
-    print("\n>>> Centralidade: ", grafo.centralidade(vertice))
-    print("\n")
+def biblioteca(grafo, numVertices):
+    print("ESCOLHA UMA FUNCIONALIDADE:\n")
+    print("(0) Para sair")
+    print("(1) Ordem do grafo")
+    print("(2) Tamnho do grafo")
+    print("(3) Retornar vizinhos do vértice")
+    print("(4) Grau do vértice")
+    print("(5) Sequência de graus")
+    print("(6) Excentricidade do vértice")
+    print("(7) Raio do grafo")
+    print("(8) Diâmetro do grafo")
+    print("(9) Centro do grafo")
+    print("(10) Busca em profundidade")
+    print("(11) Distância e caminho mínimo")
+    print("(12) Centralidade")
+    escolha = int(input(">>> "))
+    os.system("clear")
+
+    vertice = int(input("\nEscolha um vértice: "))
+    if vertice > grafo.ordem():
+        print("O vértice digitado não existe no grafo")
+        return
+    else:
+        grafo.floydWarshall(vertice)
+        if escolha == 1:
+            print("\n>>> Ordem do grafo: ", grafo.ordem())
+        elif escolha == 2:
+            print("\n>>> Tamanho do grafo: ", grafo.tamanho())
+        elif escolha == 3:
+            grafo.retornaVizinhos(vertice)
+        elif escolha == 4:
+            grafo.grauVertice(vertice)
+        elif escolha == 5:
+            grafo.sequenciaGraus()
+        elif escolha == 6:
+            print(f'\n>>> Excentricidade do vértice {vertice}: ', grafo.excentricidade(vertice))
+        elif escolha == 7:
+            print("\n>>> Raio do grafo: ", grafo.raio())
+        elif escolha == 8:
+            print("\n>>> Diâmetro do grafo: ", grafo.diametro())
+        elif escolha == 9:
+            print("\n>>> Centro do grafo: ", grafo.centro())
+        elif escolha == 10:
+            print("\n>>> Busca em profundidade: ")
+        elif escolha == 11:
+            print("\n>>> Distância e caminho mínimo\n")
+            for i in range(numVertices):
+                print(f'>>>Caminho minimo entre {vertice} e {i + 1}: {grafo.imprimeCaminho(vertice, i + 1)}')
+            print("\n")
+            grafo.imprimeDistancia(vertice)
+        elif escolha == 12:
+            print("\n>>> Centralidade: ", grafo.centralidade(vertice))
