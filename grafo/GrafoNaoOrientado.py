4ae460be87454c3f0f8944158914372857b11c5b
Vinicius T. S. Ribeiro <viniciustadeu39@gmail.com>
2022-10-27 01:41:04 -0300

    implementação da leitura de JSON

--- grafo/GrafoNaoOrientado.py
+++ grafo/GrafoNaoOrientado.py
@@ -1,3 +1,5 @@
+import json
+
 class Grafo:
     matrizL = []
     matrizR = []
@@ -101,6 +103,19 @@ class Grafo:
 
     # def buscaProfundidade()
 
+    def converteJSON(inputFile, outputFile):
+        with open(inputFile, 'r') as arq:
+            arq = arq.read()
+            jsonFile = json.loads(arq)
+            vertices = jsonFile["data"]["nodes"]["_data"]
+            arestas = jsonFile["data"]["edges"]["_data"]
+            tamanhoGrafo = len(vertices)
+            with open(outputFile, 'w') as out:
+                out.write(f'{str(tamanhoGrafo)}\n')
+                for idAresta in arestas:
+                    linha = f'{str(arestas[idAresta]["from"])} {str(arestas[idAresta]["to"])} {str(arestas[idAresta]["label"])}\n'
+                    out.write(linha)
+            print("Salvo como: " + outputFile)
 
     def centralidade(self, vertice):
         if self.flag == 1:
